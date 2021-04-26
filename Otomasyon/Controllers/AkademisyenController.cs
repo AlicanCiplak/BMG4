6583dd5c5eacc8c1317e77eb896422e7d699e427
AlicanCiplak <74678820+AlicanCiplak@users.noreply.github.com>
2021-04-26 21:48:16 +0300

    7.HAFTA

---
+++ Otomasyon/Controllers/AkademisyenController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Otomasyon.Models.Entity;
+
+namespace Otomasyon.Controllers
+{
+    public class AkademisyenController : Controller
+    {
+        // GET: Akademisyen
+        ÖğrenciBilgiSistemiEntities db = new ÖğrenciBilgiSistemiEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Listele()
+        {
+            var id =(int)Session["Hocaİd"];
+            var liste = db.İliski.Where(x => x.HocaİD == id).ToList();
+            return View(liste);
+        }
+        public ActionResult NotVer(int id,İliski p)
+        {
+            var ogr = db.İliski.Find(id);
+            ogr.Vize = p.Vize;
+            ogr.Final = p.Final;
+            db.SaveChanges();
+            return View();
+        }
+
+    
+    }
+}
