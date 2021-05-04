7f5930cf0e815e890211d962921eab681841a021
AlicanCiplak <74678820+AlicanCiplak@users.noreply.github.com>
2021-05-04 14:31:38 +0300

    8.HAFTA

---
+++ Otomasyon/Controllers/ÖgrenciController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Otomasyon.Models.Entity;
+
+namespace Otomasyon.Controllers
+{
+    public class ÖgrenciController : Controller
+    {
+        ÖğrenciBilgiSistemiEntities db = new ÖğrenciBilgiSistemiEntities();
+        // GET: Ögrenci
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Not()
+        {
+            var id = (int)Session["id"];
+            var ogrenci = db.İliski.Where(x => x.ÖgrenciİD == id).ToList();
+
+            return View();
+        }
+    }
+}
